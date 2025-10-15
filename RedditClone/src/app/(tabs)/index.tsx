181476f345c44641eeb7e604c9d5543043bdfc75
Harmans <harmans6282@gmail.com>
2025-10-15 14:46:15 +0530

    Home screen finished

--- RedditClone/src/app/(tabs)/index.tsx
+++ RedditClone/src/app/(tabs)/index.tsx
@@ -1,47 +1,18 @@
-import { Image, Text, View, StyleSheet } from "react-native";
+import { Image, Text, View, StyleSheet, FlatList } from "react-native";
 import posts from "../../../assets/data/posts.json";
-import { formatDistanceToNowStrict } from "date-fns";
+import PostListItem from "../../components/PostListItem";
 
 export default function HomeScreen() {
-  const post = posts[0];
   return (
-    <View style={{ paddingHorizontal: 15, paddingVertical: 10 }}>
-      {/* POST HEADER */}
-      <View style={{ flexDirection: "row", gap: 10 }}>
-        <Image
-          source={{ uri: post.group.image }}
-          style={styles.image}
-          alt="image"
-        />
-        <Text style={{ fontWeight: "bold" }}>{post?.group?.name}</Text>
-        <Text style={{ color: "grey" }}>
-          {formatDistanceToNowStrict(new Date(post?.created_at))}
-        </Text>
-        <View style={{ marginLeft: "auto" }}>
-          <Text style={styles.joinButtonText}>Join</Text>
-        </View>
-      </View>
+    <View>
+      {/* {posts.map((p) => (
+        <PostListItem post={posts[0]} />
+      ))} */}
 
-      {/* CONTENT */}
-
-      <Text>{post?.title}</Text>
-      <Image
-        source={{ uri: post?.image }}
-        style={{ width: "100%", aspectRatio: 4 / 3, borderRadius: 15 }}
-        alt="image"
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <PostListItem post={item} />}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  joinButtonText: {
-    backgroundColor: "#0d469b",
-    color: "white",
-    paddingVertical: 3,
-    paddingHorizontal: 8,
-    borderRadius: 10,
-  },
-
-  image: { width: 23, height: 23, borderRadius: 10 },
-});
