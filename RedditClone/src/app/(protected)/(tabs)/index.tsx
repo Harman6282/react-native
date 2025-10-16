7167ceca3d017cb1a17f0714c5363f34651d9601
Harmans <harmans6282@gmail.com>
2025-10-16 16:35:22 +0530

    clerk authentication

--- RedditClone/src/app/(protected)/(tabs)/index.tsx
+++ RedditClone/src/app/(protected)/(tabs)/index.tsx
@@ -1,14 +1,10 @@
 import { Image, Text, View, StyleSheet, FlatList } from "react-native";
-import posts from "../../../assets/data/posts.json";
-import PostListItem from "../../components/PostListItem";
+import posts from "../../../../assets/data/posts.json";
+import PostListItem from "../../../components/PostListItem";
 
 export default function HomeScreen() {
   return (
     <View>
-      {/* {posts.map((p) => (
-        <PostListItem post={posts[0]} />
-      ))} */}
-
       <FlatList
         data={posts}
         renderItem={({ item }) => <PostListItem post={item} />}
